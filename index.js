0d4b5962a352876cf5adc3498f2b104173d69e51
lipy <yao50cn@163.com>
2018-07-09 17:19:34 +0800

    1.上传工程 。

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by licp on 2016-11-28.
+ */
+
+
+
+module.exports =
+{
+    eurekaClient     :    require('./lib/eurekaClient/eurekaClient'),
+    eurekaClientConfig     :    require('./lib/eurekaClient/eurekaClientConfig'),
+};
